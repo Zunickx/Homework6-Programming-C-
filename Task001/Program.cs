b0c1e9cbecfad107f1dafce39e6e08b25f790d59
Zunickx <Zunickx@gmail.com>
2022-08-28 15:51:44 +0300

    Task001 Какое кол-во чисел ввел пользователь больше 0

---
+++ Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Введите числа через запятую: ");
+
+int[] numArray = Array.ConvertAll(Console.ReadLine()!.Split(','), int.Parse); // строку положили в массив, 
+// разбили строку на подстроки при помощи метода Split (каждый символ в строке стал подстрокой, при этом запятая стала разделителем на подстроки), 
+// заменили строчные значения на числовые при помощи int.Parse
+Console.WriteLine(numArray);
+
+void Enumeration()
+{
+    int count = 0;
+
+    for(int i = 0; i < numArray.Length; i++)
+    {
+        if(numArray[i] > 0)
+        count++;
+    }
+
+    Console.WriteLine($"Количество элементов больше 0: {count}");
+}
+
+Enumeration();
